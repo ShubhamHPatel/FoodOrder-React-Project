bbcb52ff03d4dac11a3906b27e5653c1dffe922b
Shubham Patel <shubhamhpatel5299@gmail.com>
2023-03-20 21:50:15 +0530

    Basic contact form done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
 
 import styles from "../scss/contact.module.scss";
+import Button from "./Button";
 
 const Contact = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   return (
     <section className={styles.contact}>
-      <form>
-        <div className={styles.inputField}>
-            <input
+      <div className={styles.contactHead}>
+        <h2 className={styles.contactHeading}>Contact Us</h2>
+        <p className={styles.contactSubHeading}>
+          Provide us your valuable feedback
+        </p>
+      </div>
+      <form className={styles.contactForm}>
+        <div className={styles.inputFields}>
+          <input
+            className={styles.inputEmail}
             placeholder="Email"
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <textarea placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)} />
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <textarea
+            className={styles.inputMessage}
+            placeholder="Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
         </div>
-
-        <input type="submit" />
+        <Button value="Send" />
       </form>
     </section>
   );
